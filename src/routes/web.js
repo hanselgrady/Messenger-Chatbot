86f3693ef3f1f6d44e58f57b315cd80f085982ba
Hansel Grady <13518140@std.stei.itb.ac.id>
2021-04-17 23:21:44 +0700

    Adding webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {hpcontroller} from "../controllers/homepageController.js";
+import {cbcontroller} from "../controllers/chatBotController.js"
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", hpcontroller.getHomepage);
+    router.get("/webhook", cbcontroller.getWebhook);
+    router.post("/webhook", cbcontroller.postWebhook);
 
     return app.use("/", router);
 };
